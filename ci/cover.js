32c5cc3ea1260d7a30394f76c9e87d13cd1b6788
Taka Okunishi <okunishitaka.com@gmail.com>
2016-07-21 08:57:17 +0900

    Update deps

--- ci/cover.js
+++ ci/cover.js
@@ -9,12 +9,10 @@
 process.chdir(`${__dirname}/..`)
 
 const { runTasks } = require('ape-tasking')
-const apeCovering = require('ape-covering')
+const { coverage } = require('amocha')
 
 runTasks('cover', [
-  () => apeCovering.measureCoverage('_mocha', [
-    'test/*_test.js', '-t', 30000
-  ], {
+  () => coverage('test/*_test.js', {
     dir: 'coverage'
   })
 ], true)
